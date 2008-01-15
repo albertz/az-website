74fc782250e6dbade74426f04b1392f25672016a
root <root@9c66d181-f5b8-4046-a4f4-809229a604e6>
2008-01-15 14:26:52 +0000

    english description for projects/pics
    git-svn-id: file:///var/svn/Website@25 9c66d181-f5b8-4046-a4f4-809229a604e6

--- projects/pics/main.en.php
+++ projects/pics/main.en.php
@@ -2,38 +2,63 @@
 <a href="?lang=de">Fuer die deutsche Version hier klicken.</a>
 </p>
 
+<h1>Description</h1>
 <p>
-<b>Beschreibung</b><br>
-Es handelt sich um eine einzige PHP-Datei, welche als Ersatz fuer ein
-Directory-Listing eines Verzeichnisses mit Bildern als Inhalt gedacht wurde.
-Der PHP-Skript kuemmert sich sowohl um die Auflistung, als auch um die
-Erstellung der Thumbnails (Vorschaubilder) und auch um die eigentliche
-Darstellung der eigentlichen Bilder (in wahlweise verschiedenen
-Qualitaetsstufen).
+This script basically shows the content of a directory with thumbnailed
+versions of the included images or it shows different quality versions of an
+image. The main intention was to be very fast and easy.
 </p>
 
+<p><b>Live Demonstration</b>:
+<a href="http://www.az2000.de/pics">my picture collection</a></p>
+
+<h1>Cache</h1>
 <p>
-<b>Installation (auf einem Apache-Server)</b><br>
-Das Skript liegt bei mir unter <i>/images/pics.php</i>. Das Skript muss evtl.
-noch angepasst werden, um anderweitig zu laufen (sollte nicht weiter
-schwierig sein, werde mich bald selbst mal daran setzen). Danach wird durch
-einen kleinen Hack in der <i>.htaccess</i>-Datei eines entsprechenden
-Verzeichnisses erreicht, dass dieses Verzeichnis absofort ueber den PHP-Skript
-angezeigt wird. Wie folgt sieht beispielsweise der entsprechende Auszug meiner
-<i>.htaccess</i> aus:
+The recent version also includes a caching system for the images. In the past,
+the preview-image was always generated on-the-fly which is of course very
+slow (imagine a directory with 100 pictures in it and on each request the
+script had to generate 100 thumbnails).<br>
+The PHP-script will save the generated preview-image in <i>/var/tmp/pics/</i>
+at the moment, though you can easily change this. It generates the pictures
+the first time you request it and uses the cache after (if it is not older
+than the image).
+</p>
+
+<h1>Installation</h1>
+<p>
+I use some URL-rewrite-rules to map the URL (like ".../pics/finland/"
+or "../pics/picture.jpg")
+to the PHP-script. In <i>lighttpd</i>, these are the following rules:
+</p>
+
 <pre>
-Options -Indexes
-ErrorDocument 403 /images/pics.php
+url.rewrite = (
+    "^/pics/(.*)\?get" => "/pics/$1",
+    "^/pics/(.*)/\?(.*)type=pic(.*)" => "/pics/pic.cml?dir=$1&$2$3",
+    "^/pics/(.*)/\?(.*)" => "/pics/pics.php?dir=$1&$2",
+    "^/pics/(.*)\?(.*)" => "/pics/pics.php?dir=$1&$2",
+    "^/pics/(.*)" => "/pics/pics.php?dir=$1" )
 </pre>
-Ich hab das ganze so gemacht, da ich im Prinzip zu faul war, mich durch
-viele Texte durchwuehlen zu muessen, um rauszukriegen, wie man das eigentlich
-ellegant macht. Falls jemand mir das kurz schreiben kann, wie ich eigentlich
-die <i>.htaccess</i> anpassen sollte, kann das gerne tun. Ich werde das dann
-an dieser Stelle mit einer entsprechend angepassten PHP-Datei
-veroeffentlichen.
+
+<h1>mod_cml</h1>
+<p>
+I also use <i>mod_cml</i> as you can see from the rules. This increases
+the speed a lot, especially in the case of a big amount of pictures in
+a directory. The <i>pic.cml</i> script basically checks if there is
+already a cached version of the requested image and if this is up-to-date
+and in this case, it just returns this cached image. In all other cases
+it runs the PHP-script.<br>
+I have also added this routine in the PHP-script in case you don't want
+to use <i>mod_cml</i> but though you want the use of the caches. Though
+as I said, PHP is much slower by doing this and forwarding the data of
+the cached image. It should be sufficient in the rewrite-rules to
+replace "pic.cml" with "pics.php".
 </p>
 
+<h1>Downloads</h1>
 <p>
-<b>Download</b><br>
-<a href="/images/pics.php.txt">Source Code (PHP)</a>
+<b>Source Code</b>:
+<a href="http://www.az2000.de/viewvc/Website/pics/pics.php?view=markup">pics.php</a>
+<a href="http://www.az2000.de/viewvc/Website/pics/pic.cml?view=markup">pic.cml</a>
 </p>
+
