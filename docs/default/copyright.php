b703dcaef6ff8105976f1055cf0ec891a94e4664
root <root@9c66d181-f5b8-4046-a4f4-809229a604e6>
2008-03-05 03:54:50 +0000

    typo
    git-svn-id: file:///var/svn/Website@31 9c66d181-f5b8-4046-a4f4-809229a604e6

--- docs/default/copyright.php
+++ docs/default/copyright.php
@@ -6,7 +6,7 @@ switch($lang) {
 case "en":
 ?>
 The text published here is under the copyright of Albert Zeyer.
-Distribution are only allowed with a reference to this page.
+Distribution is only allowed with a reference to this page.
 <?php
 	break;
 case "de":
