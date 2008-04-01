5d7a317802e62a0119d753f0be3cd17754c77d07
root <root@9c66d181-f5b8-4046-a4f4-809229a604e6>
2008-04-01 18:29:21 +0000

    beschreibungsseite fuer das baby
    git-svn-id: file:///var/svn/Website@44 9c66d181-f5b8-4046-a4f4-809229a604e6

---
+++ docs/das_baby/main.php
@@ -0,0 +1,14 @@
+<h2>Beschreibung</h2>
+<p>
+Das Baby ist eine sehr alte Geschichte von mir, die ich vor vielen Jahren
+einmal begonnen habe auf einer Autofahrt in einen Urlaub. In folgenden Urlauben
+und zu sonstigen Gelegenheiten habe ich immer ein wenig die Geschichte
+erweitert.
+</p>
+<p>
+Sie beschreibt das Leben aus der Sicht eines kreativen Babies.
+</p>
+
+<h2>Downloads</h2>
+<a href="DasBaby.pdf">Das Baby (PDF)</a><br>
+<a href="DasBaby.odt">Das Baby (ODT)</a>
