7aa3b97cc3d5840c89a76889f52651de10c01bf2
root <root@9c66d181-f5b8-4046-a4f4-809229a604e6>
2008-04-03 13:31:51 +0000

    exported general functions
    git-svn-id: file:///var/svn/Website@45 9c66d181-f5b8-4046-a4f4-809229a604e6

--- projects/default/index.php
+++ projects/default/index.php
@@ -1,5 +1,6 @@
 <?php
 	include("../../sql.php");
+	include("../../functions.php");
 	include("../default/text.php");
 	
 	// TODO: does this handling of request-variables in a special file
@@ -39,46 +40,6 @@
 			$file = "main.php";
 	}
 
-	function FileExt($fname) {
-		$pos = strrpos($fname, ".");
-		if($pos === false) return "";
-		return strtolower(substr($fname, $pos + 1));
-	}
-
-	function ListFiles($dir) {
-		$d = dir($dir);
-		$files_count = 0;
-		while(false !== ($entry = $d->read())) {
-			if($entry != "" && substr($entry, 0, 1) != ".")
-			switch($entry) {
-			case "index.php":
-			case "desc.txt":
-			case "main.php":
-			case "mysql.name":
-			case "mysql.description":
-			case "mysql.date":
-			case "mysql.marking":
-				break;
-			default:
-				switch(FileExt($entry)) {
-				case "jpg":
-				case "jpeg":
-				case "png":
-				case "gif":
-				case "svg":
-					echo '<img src="'.$dir.'/'.$entry.'"><br>';
-					break;
-				default:
-					echo '<a href="'.$dir.'/'.$entry.'">'.$entry.'</a><br>';
-				}
-				echo "\n";
-				$files_count++;
-			}
-		}
-		if($files_count == 0)
-			echo "No files.";
-		return;
-	}
 	
 	if(file_exists($file)) {
 		$showing_main_file = true;
