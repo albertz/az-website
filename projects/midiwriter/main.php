da9d3bcf92dd50966693d2e5bfdded0bd056770d
root <root@9c66d181-f5b8-4046-a4f4-809229a604e6>
2008-05-29 16:42:02 +0000

    midiwriter added to collection
    git-svn-id: file:///var/svn/Website@60 9c66d181-f5b8-4046-a4f4-809229a604e6

---
+++ projects/midiwriter/main.php
@@ -0,0 +1,13 @@
+<p>
+MidiWriter is a framework for algorithmic music composition.
+</p>
+
+<p>
+The project is hosted by SourceForge. Please visit the official website:
+<a href="http://midiwriter.sourceforge.net"><b>midiwriter.sourceforge.net</b></a>
+</p>
+
+<?php
+	// for other information below
+	$lang="en";
+?>
--- projects/openlierox/main.php
+++ projects/openlierox/main.php
@@ -3,12 +3,8 @@ This game is under the development of Dark Charlie and Albert Zeyer.
 </p>
 
 <p>
-Sorry guys, not that much content here :)
-</p>
-
-<p>
-Fore more detailed information, take a look at the official website:<br>
-<a href="http://openlierox.sourceforge.net">openlierox.sourceforge.net</a>
+The project is hosted by SourceForge. Please visit the official website:
+<a href="http://openlierox.sourceforge.net"><b>openlierox.sourceforge.net</b></a>
 </p>
 
 <p>
