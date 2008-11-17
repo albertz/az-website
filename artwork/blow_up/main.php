23a629e001f13062dd03c63da56aad21608a7fd5
root <root@9c66d181-f5b8-4046-a4f4-809229a604e6>
2008-11-17 23:54:04 +0000

    git-svn-id: file:///var/svn/Website@84 9c66d181-f5b8-4046-a4f4-809229a604e6

---
+++ artwork/blow_up/main.php
@@ -0,0 +1,13 @@
+<center>
+
+<p>
+This is a blow-up.
+</p>
+
+<p>This picture is dedicated to Nobody (as far as I know, I actually have no idea where it is).</p>
+
+<p>
+<img src="blow_up.png" width="840" height="1164" border="0">
+</p>
+
+</center>
