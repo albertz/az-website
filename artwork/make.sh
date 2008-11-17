cf19a2f05cff3eedf5a00cfac8c7da7bb414fea1
root <root@9c66d181-f5b8-4046-a4f4-809229a604e6>
2008-11-17 18:50:34 +0000

    two worlds
    git-svn-id: file:///var/svn/Website@79 9c66d181-f5b8-4046-a4f4-809229a604e6

--- artwork/make.sh
+++ artwork/make.sh
@@ -3,8 +3,6 @@
 SNAME=$1
 [ "$SNAME" == "" ] && echo "please specify the artwork short name" && exit 1
 [ ! -d "$SNAME" ] && echo "artwork $SNAME does not exist" && exit 1
-[ ! -e "$SNAME/$SNAME.big.jpg" ] && echo "picture $SNAME.big.jpg does not exist" && exit 1
-[ ! -e "$SNAME/$SNAME.low.jpg" ] && echo "picture $SNAME.low.jpg does not exist" && exit 1
 
 function image_w() {
 	sips "$1" -g pixelWidth | grep pixelWidth | grep -E "[0-9]+" -o
@@ -14,19 +12,48 @@ function image_h() {
 	sips "$1" -g pixelHeight | grep pixelHeight | grep -E "[0-9]+" -o
 }
 
+for SVAR in name date description marking; do
+	if [ -e $SNAME/mysql.$SVAR ]; then
+		echo "mysql.$SVAR is already set"
+	else
+		VAL=""
+		echo -n "$SVAR"
+		case $SVAR in
+			date) echo -n "(YYYY-MM-DD)";;
+			marking) echo -n "(0-120)";;
+		esac
+		echo -n ": "
+		read VAL
+		echo "$VAL" > $SNAME/mysql.$SVAR
+		svn add $SNAME/mysql.$SVAR
+	fi
+done
+
+cd $SNAME
+if [ ! -e index.php ]; then
+	ln -s ../default/index.php
+	svn add index.php
+fi
+cd ..
 
 if [ -e $SNAME/main.php ]; then
 	echo "$SNAME/main.php already exists, ignoring template generation"
 else
+
+# [ ! -e "$SNAME/$SNAME.big.jpg" ] && echo "picture $SNAME.big.jpg does not exist" && exit 1
+# [ ! -e "$SNAME/$SNAME.low.jpg" ] && echo "picture $SNAME.low.jpg does not exist" && exit 1
 	
 VARS="$(grep -E "%[A-Z0-9]+%" -o default/template_main.php | uniq)"
 cp default/template_main.php $SNAME/main.php
-
 
 for VAR in $VARS; do
 	VAL=""
 	case "$VAR" in
 		"%SNAME%") VAL=$SNAME;;
+		"%SDESC%") VAL="$(cat $SNAME/mysql.description)";;
+		"%NAME%") VAL="$(cat $SNAME/mysql.name)";;
+		"%MARKING%") VAL="$(cat $SNAME/mysql.marking)";;
+		"%DATE%") VAL="$(cat $SNAME/mysql.date)";;
 		"%W%") VAL=$(image_w $SNAME/$SNAME.low.jpg);;
 		"%H%") VAL=$(image_h $SNAME/$SNAME.low.jpg);;
 		*)
