76c0a4c1f2d1f4ec43d50e6e29789aa30341d12f
root <root@9c66d181-f5b8-4046-a4f4-809229a604e6>
2008-11-17 23:15:51 +0000

    git-svn-id: file:///var/svn/Website@82 9c66d181-f5b8-4046-a4f4-809229a604e6

---
+++ artwork/merging/main.php
@@ -0,0 +1,16 @@
+<center>
+
+<p>
+There is some stuff going on.
+</p>
+
+<p>This picture is dedicated to Lisa.</p>
+
+<p>
+(Click to view the original picture.)<br>
+<a href="merging.big.jpg">
+<img src="merging.low.jpg" width="800" height="550" border="0">
+</a><br>
+</p>
+
+</center>
