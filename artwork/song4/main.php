a118fa2dd491b1ed637a2a08654a9a56e5edb7f3
root <root@9c66d181-f5b8-4046-a4f4-809229a604e6>
2008-11-18 22:56:01 +0000

    more details for song4
    git-svn-id: file:///var/svn/Website@101 9c66d181-f5b8-4046-a4f4-809229a604e6

--- artwork/song4/main.php
+++ artwork/song4/main.php
@@ -3,9 +3,17 @@ This song belongs to the <a href="http://midiwriter.sourceforge.net">MidiWriter<
 </p>
 
 <p>
+This song has a long revision history and I overworked it again and again. I am not that happy with the final version after relistening to it some time later. After some comparison with earlier versions, I think the revision 87 is a much better version of this song. It is much simpler at this stage of the development but that sounds better than the overcomplicated final version.
+</p>
+
+<p>
+I will not make any further modifications to this song, though some further songs will probably base somehow on these architecture and ideas.
+</p>
+
+<p>
+- <a href="http://midiwriter.sourceforge.net/#song4">description</a><br>
 - <a href="http://midiwriter.svn.sourceforge.net/viewvc/midiwriter/song4rev87.mid?view=co">Midi file (rev87)</a><br>
 - <a href="http://downloads.sourceforge.net/midiwriter/song4rev87.ogg">Ogg file (rev87)</a><br>
-- <a href="http://midiwriter.sourceforge.net/#song4">description</a><br>
 - <a href="http://midiwriter.svn.sourceforge.net/viewvc/midiwriter/song4.mid?view=co">Midi file</a><br>
 - <a href="http://downloads.sourceforge.net/midiwriter/song1.ogg">Ogg file</a>
 </p>
