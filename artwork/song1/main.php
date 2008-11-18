b21abe40393b471c0e959da1f96818a724932a61
root <root@9c66d181-f5b8-4046-a4f4-809229a604e6>
2008-11-18 22:46:42 +0000

    small fixes
    git-svn-id: file:///var/svn/Website@99 9c66d181-f5b8-4046-a4f4-809229a604e6

--- artwork/song1/main.php
+++ artwork/song1/main.php
@@ -4,12 +4,12 @@ This song belongs to the <a href="http://midiwriter.sourceforge.net">MidiWriter<
 
 <p>
 - <a href="http://midiwriter.sourceforge.net/#song1">description</a><br>
-- <a href="http://midiwriter.svn.sourceforge.net/viewvc/midiwriter/song1.mid?view=co">Midi file</a>
+- <a href="http://midiwriter.svn.sourceforge.net/viewvc/midiwriter/song1.mid?view=co">Midi file</a><br>
 - <a href="http://downloads.sourceforge.net/midiwriter/song1.ogg">Ogg file</a>
 </p>
 
 <p>
 This song is part of the compilation <a href="http://midiwriter.sourceforge.net/#experimentalsonatas1">Experimental Sonatas 1</a>.<br>
 - <a href="http://downloads.sourceforge.net/midiwriter/ExperimentalSonatas1.zip">Ogg version</a><br>
-- <a href="http://downloads.sourceforge.net/midiwriter/ExperimentalSonatas1_MP3.zip">MP3 version</a><br>
+- <a href="http://downloads.sourceforge.net/midiwriter/ExperimentalSonatas1_MP3.zip">MP3 version</a>
 </p>
--- artwork/song4/main.php
+++ artwork/song4/main.php
@@ -3,13 +3,15 @@ This song belongs to the <a href="http://midiwriter.sourceforge.net">MidiWriter<
 </p>
 
 <p>
+- <a href="http://midiwriter.svn.sourceforge.net/viewvc/midiwriter/song4rev87.mid?view=co">Midi file (rev87)</a><br>
+- <a href="http://downloads.sourceforge.net/midiwriter/song4rev87.ogg">Ogg file (rev87)</a><br>
 - <a href="http://midiwriter.sourceforge.net/#song4">description</a><br>
-- <a href="http://midiwriter.svn.sourceforge.net/viewvc/midiwriter/song4.mid?view=co">Midi file</a>
+- <a href="http://midiwriter.svn.sourceforge.net/viewvc/midiwriter/song4.mid?view=co">Midi file</a><br>
 - <a href="http://downloads.sourceforge.net/midiwriter/song1.ogg">Ogg file</a>
 </p>
 
 <p>
 This song is part of the compilation <a href="http://midiwriter.sourceforge.net/#experimentalsonatas1">Experimental Sonatas 1</a>.<br>
 - <a href="http://downloads.sourceforge.net/midiwriter/ExperimentalSonatas1.zip">Ogg version</a><br>
-- <a href="http://downloads.sourceforge.net/midiwriter/ExperimentalSonatas1_MP3.zip">MP3 version</a><br>
+- <a href="http://downloads.sourceforge.net/midiwriter/ExperimentalSonatas1_MP3.zip">MP3 version</a>
 </p>
