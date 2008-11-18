1881aa14575f2813b5fd983896e7f64f6983d9df
root <root@9c66d181-f5b8-4046-a4f4-809229a604e6>
2008-11-18 23:24:12 +0000

    some alien
    git-svn-id: file:///var/svn/Website@102 9c66d181-f5b8-4046-a4f4-809229a604e6

---
+++ artwork/alien/main.php
@@ -0,0 +1,16 @@
+<center>
+
+<p>
+That is what aliens do all the time.
+</p>
+
+<p>This picture is dedicated to somebody (but I forgot whom).</p>
+
+<p>
+(Click to view the original picture.)<br>
+<a href="alien.big.jpg">
+<img src="alien.low.jpg" width="800" height="499" border="0">
+</a><br>
+</p>
+
+</center>
