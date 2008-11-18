45b35df5544e4f2eeccfc2ab725d7a615935b429
root <root@9c66d181-f5b8-4046-a4f4-809229a604e6>
2008-11-18 01:05:39 +0000

    git-svn-id: file:///var/svn/Website@90 9c66d181-f5b8-4046-a4f4-809229a604e6

--- artwork/living_technic/main.php
+++ artwork/living_technic/main.php
@@ -1,16 +1,9 @@
 <center>
 
-<p>
-
-</p>
-
 <p>This picture is dedicated to living things.</p>
 
 <p>
-(Click to view the original picture.)<br>
-<a href="living_technic.big.jpg">
-<img src="living_technic.low.jpg" width="" height="" border="0">
-</a><br>
+<img src="living_technic.low.jpg" width="700" height="775" border="0">
 </p>
 
 </center>
